33eba612aab448eb0a3e5bb9caeef24ae174d485
Hector Alfaro <halfaro1@github.com>
2016-06-08 15:39:19 -0400

    py

--- test_sample.py
+++ test_sample.py
@@ -2,29 +2,10 @@
 import geojson
 from path import path
 
-def func(x):
-    return x + 1
-
-def test_answer():
-    assert func(3) == 5
-
 def test_geojson():
     try:
         s = path("class-pins.topojson").bytes()
         rawJson = geojson.loads(s)
-        return True
+        assert True
     except:
-        return False
-    #print s
-
-    #my_point = geojson.Point((43.24, -1.532))
-    #dump = geojson.dumps(s, sort_keys=True)
-    #print dump
-    #print geojson.loads( s )
-
-    #validation = geojson.is_valid( rawJson )
-    #print validation
-
-
-
-print test_geojson();
+        assert False
