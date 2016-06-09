3d9c0d815015b1891acff37e242f1264df901807
Hector Alfaro <halfaro1@github.com>
2016-06-09 17:33:43 -0400

    test

--- deploy-script.sh
+++ deploy-script.sh
@@ -14,12 +14,11 @@ rm class-pins.topojson
 mv newmap.topojson class-pins.topojson
 
 # deploy
-git add -A
+git add class-pins.topojson
 git commit -m "Deploy to Github Pages"
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" HEAD:gh-pages
 
 rm mymap.topojson
-touch mymap.topojson
 git add -A
 git commit -m "reset"
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" HEAD:master
