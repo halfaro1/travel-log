daa9f1dc2b2ce323ddebd4f04071583d26e13ae4
Hector Alfaro <halfaro1@github.com>
2016-06-09 19:52:18 -0400

    add ssh key

--- deploy-script.sh
+++ deploy-script.sh
@@ -5,6 +5,11 @@ git status
 git remote -v
 ls -lah
 
+openssl aes-256-cbc -K $encrypted_3af51e426c8b_key -iv $encrypted_3af51e426c8b_iv -in traviskey.enc -out traviskey -d
+eval "$(ssh-agent -s)" #start the ssh agent
+chmod 600 traviskey # this key should have push access
+ssh-add traviskey
+
 # config
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
@@ -16,4 +21,5 @@ mv newmap.topojson class-pins.topojson
 # deploy
 git add class-pins.topojson
 git commit -m "Deploy to Github Pages [ci skip]"
-git push --force --quiet "https://git@github.com/${GITHUB_REPO}.git" HEAD:gh-pages
+#git push --force --quiet HEAD:gh-pages
+git push --force --quiet "git@github.com:${GITHUB_REPO}.git" HEAD:gh-pages
