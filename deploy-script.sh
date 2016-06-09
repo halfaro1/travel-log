f33761cd777303f871343ad8f5ec5b227687e78b
Hector Alfaro <halfaro1@github.com>
2016-06-09 16:44:48 -0400

    test

--- deploy-script.sh
+++ deploy-script.sh
@@ -12,10 +12,14 @@ git config --global user.name "Travis CI"
 # build
 rm class-pins.topojson
 mv newmap.topojson class-pins.topojson
-touch newmap.topojson
 
 # deploy
 git add -A
 git commit -m "Deploy to Github Pages"
-#git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" HEAD:master
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" HEAD:gh-pages
+
+rm mymap.topojson
+touch mymap.topojson
+git add -A
+git commit -m "reset"
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" HEAD:master
