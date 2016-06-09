9c6b077582d814112917bcd0fc9d5bed6c1f8609
Hector Alfaro <halfaro1@github.com>
2016-06-09 10:41:40 -0400

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -17,5 +17,5 @@ touch wohoo.md
 # deploy
 git add -A
 git commit -m "Deploy to Github Pages"
-git push
-#git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" HEAD:master
+#git push
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" HEAD:master
