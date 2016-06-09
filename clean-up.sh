34ef75910fefc90d5781bc47474c0baa7d1f9117
Hector Alfaro <halfaro1@github.com>
2016-06-09 18:30:44 -0400

    test

--- clean-up.sh
+++ clean-up.sh
@@ -1,4 +1,3 @@
-rm mymap.topojson
-git add mymap.topojson
+git rm mymap.topojson
 git commit -m "reset [ci skip]"
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" HEAD:master
