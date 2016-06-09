fdfeea31c36091de0cc288e1f73466858856b79c
Hector Alfaro <halfaro1@github.com>
2016-06-09 19:56:51 -0400

    fix [skip ci]

--- clean-up.sh
+++ clean-up.sh
@@ -1,3 +1,3 @@
 git rm mymap.topojson
 git commit -m "reset [ci skip]"
-git push --force --quiet "https://git@github.com/${GITHUB_REPO}.git" HEAD:master
+git push --force --quiet "git@github.com:${GITHUB_REPO}.git" HEAD:master
